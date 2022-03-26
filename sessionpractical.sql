0ea12bad4910a0a64e7c5ef8457f09535d362c21
Masontysom <meethmali24@gmail.com>
2022-03-27 01:28:54 +0530

    database

---
+++ sessionpractical.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 26, 2022 at 07:53 PM
+-- Server version: 8.0.26
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sessionpractical`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signin`
+--
+
+CREATE TABLE `signin` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `signin`
+--
+
+INSERT INTO `signin` (`id`, `name`, `password`) VALUES
+(9, 'meeth', '123456'),
+(10, 'meet', '1234'),
+(11, 'examination', 'Meeth242004'),
+(12, 'MEETHMALI', '12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `signin`
+--
+ALTER TABLE `signin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `signin`
+--
+ALTER TABLE `signin`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
